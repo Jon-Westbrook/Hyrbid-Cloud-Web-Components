51cde7493ae1df7c09b3da9296f5fffdccca6b07
Mateu Aguiló Bosch (e0ipso) <mateu@mateuaguilo.com>
2021-10-13 22:35:03 +0200

    fix: use correct paths in temp index

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import store from './widgets/trust-radius/lib/redux/store';
-import TrustRadius from './widgets/trust-radius/components/TrustRadius';
+import store from './apps/trust-radius/lib/redux/store';
+import TrustRadius from './apps/trust-radius/components/TrustRadius';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <TrustRadius
-        palette="light"
         useGoogleStars={true}
         trustRadiusId="5d9df1232f82250032904eb1"
       />
