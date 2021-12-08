078f4d5a6fcddd80d39e762116ac484116303f9a
Mateu Aguiló Bosch (e0ipso) <mateu@mateuaguilo.com>
2021-12-08 09:57:34 +0100

    ci: avoid deleting widget code

--- scripts/ci/upload-widgets-to-cos.sh
+++ scripts/ci/upload-widgets-to-cos.sh
@@ -5,6 +5,8 @@ set -e
 SCRIPT_RELATIVE_DIR=$(dirname "${BASH_SOURCE[0]}")
 PROJECT_ROOT=$(dirname "$(dirname "${SCRIPT_RELATIVE_DIR}")")
 
-source "${PROJECT_ROOT}/scripts/ci/delete-from-cos.sh" '^"widgets/)'
+# Do not remove widgets. We'll be keeping the different hashed versions. This
+# is to avoid deleting something only to have the upload process fail. This
+# could cause an outage for widgets in production.
 source "${PROJECT_ROOT}/scripts/ci/delete-from-cos.sh" '^"storybook/'
 source "${PROJECT_ROOT}/scripts/ci/upload-to-cos.sh" "${PROJECT_ROOT}/widget-registry"
