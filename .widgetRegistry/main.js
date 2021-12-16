380625e2c9574b24182412c7e51df42f7ded22d0
Aguilo Bosch <mateu@lullabot.com>
2021-12-16 22:34:38 +0100

    fix: update webpack config improvements (#26)
    This contains inspiration from the Create React App webpack configuration.
    
    Refs WIDGET-9

--- .widgetRegistry/main.js
+++ .widgetRegistry/main.js
@@ -1,43 +1,41 @@
+const path = require('path');
 const CompressionPlugin = require('compression-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 const {
   constants: { BROTLI_PARAM_QUALITY },
 } = require('zlib');
-
-const additionalPlugins = [
-  new CompressionPlugin({
-    deleteOriginalAssets: true,
-    algorithm: 'brotliCompress',
-    test: /\.(js|css|svg|js\.map)$/,
-    compressionOptions: {
-      params: {
-        [BROTLI_PARAM_QUALITY]: 11,
-      },
-    },
-    threshold: 10240,
-    minRatio: 0.8,
-    filename: '[path][base]',
-  }),
-];
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
 module.exports = {
   register: ['../src/apps/**/*.widget.js'],
   webpackFinal: (config) => {
-    // Ensure we handle TypeScript using the babel loader.
-    config.module.rules[2].test = /\.(j|t)sx?$/;
+    const isEnvDevelopment = config.mode === 'development';
+    const isEnvProduction = config.mode === 'production';
+    const disableESLintPlugin = process.env.DISABLE_ESLINT_PLUGIN === 'true';
+    const emitErrorsAsWarnings = process.env.ESLINT_NO_DEV_ERRORS === 'true';
+    const hasJsxRuntime = (() => {
+      if (process.env.DISABLE_NEW_JSX_TRANSFORM === 'true') {
+        return false;
+      }
+
+      try {
+        require.resolve('react/jsx-runtime');
+        return true;
+      } catch (e) {
+        return false;
+      }
+    })();
+
     // Add the React and Typescript configurations to babel loader to process
     // .tsx files.
     let presets = config.module.rules[2].use.options.presets;
     presets.push(['@babel/preset-react', { runtime: 'automatic' }]);
-    presets.push('@babel/preset-typescript');
 
     // The default configuration adds the ts-loader, which we don't need now.
     config.module.rules[2].use.options.presets = presets;
     const babelPlugins = config.module.rules[2].use.options.plugins || [];
     babelPlugins.push('@babel/plugin-transform-runtime');
     config.module.rules[2].use.options.plugins = babelPlugins;
-    config.module.rules = config.module.rules.filter(
-      (rule) => rule.loader !== 'ts-loader',
-    );
 
     config.resolve = config.resolve || {};
     config.resolve.alias = config.resolve.alias || {};
@@ -45,7 +43,56 @@ module.exports = {
       '@formatjs/icu-messageformat-parser/no-parser';
 
     config.plugins = config.plugins || [];
-    config.plugins = [...config.plugins, ...additionalPlugins];
+    config.plugins = [
+      ...config.plugins,
+      // Generate an asset manifest file with the following content:
+      // - "files" key: Mapping of all asset filenames to their corresponding
+      //   output file so that tools can pick it up without having to parse
+      //   `index.html`
+      // - "entrypoints" key: Array of files which are included in `index.html`,
+      //   can be used to reconstruct the HTML if necessary
+      new WebpackManifestPlugin({
+        fileName: 'asset-manifest.json',
+        useEntryKeys: true,
+      }),
+      !disableESLintPlugin && new ESLintPlugin({
+        // Plugin options
+        extensions: ['js', 'mjs', 'jsx', 'ts', 'tsx'],
+        formatter: require.resolve('react-dev-utils/eslintFormatter'),
+        eslintPath: require.resolve('eslint'),
+        failOnError: !(isEnvDevelopment && emitErrorsAsWarnings),
+        context: path.resolve(__dirname, '../src'),
+        cache: true,
+        cacheLocation: path.resolve(
+          __dirname,
+          '../node_modules/.cache/.eslintcache'
+        ),
+        // ESLint class options
+        cwd: path.resolve(__dirname, '..'),
+        resolvePluginsRelativeTo: __dirname,
+        baseConfig: {
+          extends: [require.resolve('eslint-config-react-app/base')],
+          rules: {
+            ...(!hasJsxRuntime && {
+              'react/react-in-jsx-scope': 'error',
+            }),
+          },
+        },
+      }),
+      isEnvProduction && new CompressionPlugin({
+        deleteOriginalAssets: true,
+        algorithm: 'brotliCompress',
+        test: /\.(js|css|svg|js\.map)$/,
+        compressionOptions: {
+          params: {
+            [BROTLI_PARAM_QUALITY]: 11,
+          },
+        },
+        threshold: 10240,
+        minRatio: 0.8,
+        filename: '[path][base]',
+      }),
+    ].filter(Boolean);
 
     return config;
   },
