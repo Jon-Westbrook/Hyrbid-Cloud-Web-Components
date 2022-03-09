da246dfc78fe50bb42d48fb066163f605cb4122c
John Kaeser <jakaeser44@gmail.com>
2022-03-09 11:39:39 -0500

    feat(product-explorer-it-infra): remove misleading settings schema
    These came from the v1 widget, and best I can tell, they are no longer needed.

--- src/apps/product-explorer-it-infra/ProductExplorerITInfra.widget.js
+++ src/apps/product-explorer-it-infra/ProductExplorerITInfra.widget.js
@@ -5,27 +5,6 @@ module.exports = {
   status: 'stable',
   description:
     'Allows users to explore various IBM products on offer. Localized for it-infra.',
-  settingsSchema: {
-    type: 'object',
-    properties: {
-      fields: {
-        type: 'object',
-        properties: {
-          title: {
-            type: 'string',
-            title: 'Title',
-            description: '',
-          },
-          description: {
-            type: 'string',
-            title: 'Description',
-            description: '',
-          },
-        },
-      },
-      required: ['title', 'description'],
-    },
-  },
   useExternalPeerDependencies: ['react', 'react-dom', 'react-intl'],
   additionalCustomProperties: {
     webSegmentPaths: ['/cloud'],
