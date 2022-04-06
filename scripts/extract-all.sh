aee8bc8435f4fa2f70b8202dd2174d2ef6cc18e4
Aguilo Bosch <mateu@lullabot.com>
2022-04-06 22:32:36 +0200

    fix: use correct extraction command (#38)
    * fix: use correct extraction command
    
    Otherwise the extraction script will never find anything.
    
    * fix: use absolute paths

--- scripts/extract-all.sh
+++ scripts/extract-all.sh
@@ -12,8 +12,9 @@ PROJECT_ROOT=$(realpath "$(dirname "${SCRIPT_RELATIVE_DIR}")")
 
 allLanguages=("ar" "de" "en" "es" "esla" "fr" "it" "ja" "ko" "pl" "pt" "ru" "tr" "zhcn" "zhtw")
 messages_path="${PROJECT_ROOT}/src/apps/$1/locales"
+app_path="${PROJECT_ROOT}/src/apps/$1"
 
 for language in "${allLanguages[@]}"
 do
-  yarn formatjs extract "${messages_path}/messages.js*" --out-file ${messages_path}/extractedStrings/${language}.json --id-interpolation-pattern '[sha512:contenthash:base64:6]' \;
+  yarn formatjs extract "${app_path}/**/*.tsx" "${app_path}/**/*.ts" "${app_path}/**/*.jsx" "${app_path}/**/*.js" --out-file ${messages_path}/extractedStrings/${language}.json --id-interpolation-pattern '[sha512:contenthash:base64:6]' \;
 done
