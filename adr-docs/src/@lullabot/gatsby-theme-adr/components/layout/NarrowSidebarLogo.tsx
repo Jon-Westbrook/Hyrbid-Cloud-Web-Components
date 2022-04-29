4aec4ad63cf4fcc012ae9fe8397704bd61c5aa2b
Aguilo Bosch <mateu@lullabot.com>
2022-04-29 12:30:14 +0200

    feat: add support for ADRs (#47)
    Refs WIDGET-130

---
+++ adr-docs/src/@lullabot/gatsby-theme-adr/components/layout/NarrowSidebarLogo.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import logo from '../../../../images/logo.svg';
+
+const NarrowSidebarLogo: React.FC = () => (
+  <img src={logo} className="w-auto w-16" alt="Logo" />
+);
+
+export default NarrowSidebarLogo;
---
+++ scripts/ci/upload-adrs-to-cos.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+# Calculate the absolute path to the project root.
+SCRIPT_RELATIVE_DIR=$(dirname "${BASH_SOURCE[0]}")
+PROJECT_ROOT=$(dirname "$(dirname "${SCRIPT_RELATIVE_DIR}")")
+
+# Do not remove widgets. We'll be keeping the different hashed versions. This
+# is to avoid deleting something only to have the upload process fail. This
+# could cause an outage for widgets in production.
+source "${PROJECT_ROOT}/scripts/ci/delete-from-cos.sh" '^"architecture/'
+source "${PROJECT_ROOT}/scripts/ci/upload-to-cos.sh" "${PROJECT_ROOT}/adr-docs"
