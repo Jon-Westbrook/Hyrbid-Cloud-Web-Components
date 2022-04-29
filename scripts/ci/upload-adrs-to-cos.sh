21b2d179f251364eb61f5c8fc23470b212183e34
Mateu Aguiló Bosch (e0ipso) <mateu@mateuaguilo.com>
2022-04-29 18:39:54 +0200

    Revert "fix: do not upload all the things"
    This reverts commit 8adb3e643c57639d076d38e5eedefb94baed31d7.

--- scripts/ci/upload-adrs-to-cos.sh
+++ scripts/ci/upload-adrs-to-cos.sh
@@ -9,4 +9,4 @@ PROJECT_ROOT=$(dirname "$(dirname "${SCRIPT_RELATIVE_DIR}")")
 # is to avoid deleting something only to have the upload process fail. This
 # could cause an outage for widgets in production.
 source "${PROJECT_ROOT}/scripts/ci/delete-from-cos.sh" '^"architecture/'
-source "${PROJECT_ROOT}/scripts/ci/upload-to-cos.sh" "${PROJECT_ROOT}/wit-adrs"
+source "${PROJECT_ROOT}/scripts/ci/upload-to-cos.sh" "${PROJECT_ROOT}/adr-docs"
