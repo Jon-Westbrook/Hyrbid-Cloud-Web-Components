de1f369a46311529d7b9940763c6c74d11a98b1c
FlashGooden <Flash@heysparkbox.com>
2022-05-16 00:37:26 -0400

    feat: update locale for Redux

--- src/apps/product-explorer/lib/storyWithRedux.tsx
+++ src/apps/product-explorer/lib/storyWithRedux.tsx
@@ -3,12 +3,13 @@ import storyWithReduxDecorator from '../../../common/storyWithReduxDecorator';
 import { mapProductsToCategories } from '../../../common/product-explorer/lib/redux/slices/categoriesSlice';
 import { products } from '../assets/data/products';
 import messages from '../locales/messages';
+import { IBMLocale } from 'src/common/mapValidLocale';
 
 export const defaultFakeState: ProductExplorerState = {
   categories: mapProductsToCategories(products),
   loading: false,
   messages: messages,
-  localeCode: 'en',
+  localeCode: IBMLocale.EN,
 };
 
 export default storyWithReduxDecorator(defaultFakeState);
