ac44f00e1793e05eeb79bfb18f133f8942ba4137
FlashGooden <Flash@heysparkbox.com>
2022-05-16 10:28:35 -0400

    feat: update stories with proper components and classWrapper

--- src/apps/product-explorer/components/ProductDetail.stories.tsx
+++ src/apps/product-explorer/components/ProductDetail.stories.tsx
@@ -3,11 +3,16 @@ import { Meta, StoryFn } from '@storybook/react';
 import ProductDetail from './ProductDetail';
 import storyWithTranslation from '../lib/storyWithTranslation';
 import storyWithRedux from '../lib/storyWithRedux';
+import storyWithClassNameWrapper from 'src/common/storyWithClassNameWrapper';
 
 const stories: Meta = {
   title: 'Widgets/Product Explorer/Components/Product Detail',
   component: ProductDetail,
-  decorators: [storyWithTranslation(), storyWithRedux()],
+  decorators: [
+    storyWithTranslation(),
+    storyWithRedux(),
+    storyWithClassNameWrapper('product-explorer'),
+  ],
 };
 
 const product = {
--- src/apps/product-explorer/components/ProductsDisplay.stories.tsx
+++ src/apps/product-explorer/components/ProductsDisplay.stories.tsx
@@ -3,18 +3,24 @@ import { Meta, StoryFn } from '@storybook/react';
 import ProductExplorer from './ProductExplorer';
 import storyWithTranslation from '../lib/storyWithTranslation';
 import storyWithRedux from '../lib/storyWithRedux';
+import ProductsDisplay from './ProductsDisplay';
+import storyWithClassNameWrapper from 'src/common/storyWithClassNameWrapper';
 
 const stories: Meta = {
   title: 'Widgets/Product Explorer/Components/Products Display',
-  component: ProductExplorer,
-  decorators: [storyWithTranslation(), storyWithRedux()],
+  component: ProductsDisplay,
+  decorators: [
+    storyWithTranslation(),
+    storyWithRedux(),
+    storyWithClassNameWrapper('product-explorer'),
+  ],
 };
 
 const props = {
   linkType: 'product',
 };
 
-const Template: StoryFn = () => <ProductExplorer {...props} />;
+const Template: StoryFn = () => <ProductsDisplay {...props} />;
 export const Default = Template.bind({});
 
 export default stories;
