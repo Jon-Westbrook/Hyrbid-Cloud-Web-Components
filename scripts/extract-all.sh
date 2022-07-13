7cf3df669228f3fa25bf2cda9f3f2c632418b6f9
Aguilo Bosch <mateu@lullabot.com>
2022-07-13 19:02:49 +0200

    feat: add warning when extracting messages (#68)
    Refs: WIDGET-141

--- scripts/extract-all.sh
+++ scripts/extract-all.sh
@@ -14,6 +14,16 @@ allLanguages=("ar" "de" "en" "es" "esla" "fr" "it" "ja" "ko" "pl" "pt" "ru" "tr"
 messages_path="${PROJECT_ROOT}/src/apps/$1/locales"
 app_path="${PROJECT_ROOT}/src/apps/$1"
 
+FG_C='\033[1;37m'
+WBG_C='\033[43m'
+NO_C='\033[0m'
+echo -e ""
+echo -e "${FG_C}${WBG_C}                                                     ${NO_C}"
+echo -e "${FG_C}${WBG_C}          ALL MESSAGES WILL BE OVERWRITTEN           ${NO_C}"
+echo -e "${FG_C}${WBG_C}      Make sure to check git to see the changes      ${NO_C}"
+echo -e "${FG_C}${WBG_C}                                                     ${NO_C}"
+echo -e ""
+
 for language in "${allLanguages[@]}"
 do
   yarn formatjs extract "${app_path}/**/*.tsx" "${app_path}/**/*.ts" "${app_path}/**/*.jsx" "${app_path}/**/*.js" --out-file ${messages_path}/extractedStrings/${language}.json --id-interpolation-pattern '[sha512:contenthash:base64:6]' \;
