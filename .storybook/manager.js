8200ac85c27efc96661cb268d2a3b77148cb3b9a
Hunter MacDermut <Hunter.Macdermut@ibm.com>
2022-08-08 07:27:01 -0400

    feat: Add CSS Append Tool (#74)

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,16 @@
-import { addons } from '@storybook/addons';
+import { addons, types } from '@storybook/addons';
+import { toggleCSS } from './customTools';
 import hybridCloud from './hybridCloud';
 
 addons.setConfig({
   theme: hybridCloud,
+});
+
+addons.register('toggle_css_addon', () => {
+  addons.add('toggle_css_tool', {
+    type: types.TOOL,
+    title: 'Toggle www stylesheet',
+    match: ({ viewMode }) => !!(viewMode && viewMode.match(/^(story)$/)),
+    render: toggleCSS,
+  });
 });
