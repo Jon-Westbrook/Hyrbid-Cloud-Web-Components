a853b2d70fa8a5db9dac680f4cc8897ead0de1be
Flash Gooden <Flash.Gooden@ibm.com>
2022-08-18 14:09:07 -0400

    feat(widget-161): parallelize language compilation (#84)
    * feat(widget-161): adds parallel logic to formatjs command
    
    * feat(widget-161): add spinner functionality
    
    * feat(widget-161): Update formatting for script
    
    * feat(widget-161): update formatting for CLI
    
    * feat(widget-161): update delay variable
    
    Co-authored-by: FlashGooden <Flash.Gooden@ibm.com>

--- scripts/compile-all.sh
+++ scripts/compile-all.sh
@@ -4,10 +4,17 @@ set -e
 # Calculate the absolute path to the project root.
 SCRIPT_RELATIVE_DIR=$(dirname "${BASH_SOURCE[0]}")
 PROJECT_ROOT=$(dirname "${SCRIPT_RELATIVE_DIR}")
+source "${PROJECT_ROOT}/scripts/helpers/spinner.sh"
 
+start_spinner "Compiling"
 while IFS= read -r -d $'\0' file; do
-  echo -ne "⏳ Compiling translations for ${file}."
-  yarn formatjs compile "$file" --ast --out-file "${file//extractedStrings/compiledStrings}" > /dev/null
+  echo -e "⏳ Compiling translations for ${file}."
+  yarn formatjs compile "$file" --ast --out-file "${file//extractedStrings/compiledStrings}" > /dev/null &
   echo -e "\033[2K"
   echo -e "✅ Successfully compiled translations for ${file}."
 done < <(find "${PROJECT_ROOT}/src/apps/" -type d -name extractedStrings -exec find {} -mindepth 1 -type f -print0 -name "*.json" \;)
+wait
+stop_spinner 0
+
+echo -e "\033[2K"
+echo "✅ Sucessfully compiled all translations ✅ "
---
+++ scripts/helpers/spinner.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+function _spinner() {
+    # $1 start/stop
+    #
+    # on start: $2 display message
+    # on stop : $2 process exit status
+    #           $3 spinner function pid (supplied from stop_spinner)
+
+    case $1 in
+        start)
+            # calculate the column where spinner and status msg will be displayed
+            let column=$(tput cols)-${#2}-8
+            # display message and position the cursor in $column column
+            echo -ne ${2}
+            printf "%${column}s"
+
+            # start spinner
+            i=1
+            sp='\|/-'
+
+            while :
+            do
+                printf "\b${sp:i++%${#sp}:1}"
+                sleep 0.1
+            done
+            ;;
+        stop)
+            kill $3 > /dev/null 2>&1
+            ;;
+        *)
+            echo "invalid argument, try {start/stop}"
+            exit 1
+            ;;
+    esac
+}
+
+function start_spinner {
+    # $1 : msg to display
+    _spinner "start" "${1}" &
+    # set global spinner pid
+    _sp_pid=$!
+    disown
+}
+
+function stop_spinner {
+    # $1 : command exit status
+    _spinner "stop" $1 $_sp_pid
+    unset _sp_pid
+}
+
+export -f start_spinner stop_spinner
