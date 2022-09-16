867eb032c2cad84b3f15cf9b413bcd21d02fd20e
Jonathon Westbrook <jonathon.westbrook1@ibm.com>
2022-09-16 01:25:13 -0400

    pricing-band: adds card component

--- src/apps/pricing-band/components/PricingBand.tsx
+++ src/apps/pricing-band/components/PricingBand.tsx
@@ -1,59 +1,22 @@
-/** @jsxImportSource @emotion/react */
-import React, { ReactElement } from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from '../locales/messages';
-import { css, SerializedStyles } from '@emotion/react';
-import './PricingBand.scss';
+import React from 'react';
+import Card from './Card';
 
-export interface PricingBandProps {
-  /**
-   * One of the predefined greetings.
-   *
-   * @default Hello
-   */
-  greeting: 'Hello' | 'Hi' | 'Howdy';
-  /**
-   * The name of the user to greet.
-   */
-  name: string;
-  /**
-   * Set to true to greet with excitement.
-   *
-   * @default false
-   */
-  excitement: boolean;
-}
-
-const styles: Record<string, SerializedStyles> = {
-  container: css`
-    width: 15em;
-    background-color: white;
-    border-radius: 6px;
-    box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
-    padding: 2em;
-  `,
-  heading: css`
-    font-size: 1.4em;
-    font-weight: bold;
-    text-decoration: underline;
-    margin-bottom: 0.7em;
-  `,
-};
-
-const PricingBand = ({
-  greeting,
-  name,
-  excitement,
-}: PricingBandProps): ReactElement => (
-  <div css={styles.container}>
-    <h5 css={styles.heading}>
-      <FormattedMessage {...messages.greeting} />
-    </h5>
-    <div>
-      {greeting}, <em>{name}</em>
-      {excitement ? '!' : '.'}
+const PricingBand: React.FC = () => {
+  return (
+    <div className="pricing-band">
+      <div className="pricing-band__inner">
+        <div>
+          <Card />
+        </div>
+        <div>
+          <Card />
+        </div>
+        <div>
+          <Card />
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default PricingBand;
