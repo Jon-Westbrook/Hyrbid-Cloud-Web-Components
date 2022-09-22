f323b5531319b36f8bd2c7c0fe1fb6baf7f78e3f
Jonathon Westbrook <jonathon.westbrook1@ibm.com>
2022-09-22 11:23:35 -0400

    pricing-band: working carbon typography classes

--- src/apps/pricing-band/components/PricingBandTile.tsx
+++ src/apps/pricing-band/components/PricingBandTile.tsx
@@ -3,12 +3,12 @@ import messages from '../locales/messages';
 import { FormattedMessage } from 'react-intl';
 import { ClickableTile } from 'carbon-components-react';
 
-import './Card.scss';
+import './PricingBandTile.scss';
 
 const PricingBandTile: React.FC = () => {
   return (
     <ClickableTile href="https://www.carbondesignsystem.com/">
-      <div className="bx--card__heading">IBM FlashSystem 5000</div>
+      <div className="expressive-heading-04">FlashSystem 5015</div>
     </ClickableTile>
   );
 };
