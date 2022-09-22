91e8f9c8d0d5ee0e986c5e4ac3a58815dc6d4746
Jonathon Westbrook <jonathon.westbrook1@ibm.com>
2022-09-22 13:08:32 -0400

    pricing-band: adds tile markup and styles

--- src/apps/pricing-band/components/PricingBandTile.tsx
+++ src/apps/pricing-band/components/PricingBandTile.tsx
@@ -2,13 +2,51 @@ import React from 'react';
 import messages from '../locales/messages';
 import { FormattedMessage } from 'react-intl';
 import { ClickableTile } from 'carbon-components-react';
+import { ArrowRight16 } from '@carbon/icons-react';
 
 import './PricingBandTile.scss';
 
 const PricingBandTile: React.FC = () => {
   return (
-    <ClickableTile href="https://www.carbondesignsystem.com/">
+    <ClickableTile
+      href="https://www.carbondesignsystem.com/"
+      aria-label="Talk to us"
+    >
       <div className="expressive-heading-04">FlashSystem 5015</div>
+      <div className="price expressive-heading-03">Starting at 10, 000</div>
+      <div className="body body-short-01">
+        The 5015 is designed for budget-conscious applications without
+        compromising service, performance or capabilities.
+      </div>
+      <ul className="bullets body-short-01">
+        <li>400k IOPS per system</li>
+        <li>8.2 GB/s per system</li>
+        <li>Maximum 32 GB cache</li>
+        <li>Scale-up to 12 PB</li>
+      </ul>
+      <div className="button-group">
+        <button className="bx--tag bx--tag--green">
+          <span className="bx--tag__label">Midmarket</span>
+        </button>
+        <button className="bx--tag bx--tag--teal">
+          <span className="bx--tag__label">SAS</span>
+        </button>
+        <br />
+        <button className="bx--tag bx--tag--blue">
+          <span className="bx--tag__label">Virtualization</span>
+        </button>
+      </div>
+      <a
+        href="https://www.ibm.com/it-infrastructure/us-en/resources/storage/product-tour/spectrum-virtualize/"
+        className="bx--link bx--link-with-icon bx--link--lg"
+        data-link-type="local"
+        data-dynamic-properties='{"ctaUrl":"href"}'
+      >
+        <span className="cta-copy">
+          <span className="label">Talk to us</span>&nbsp;&nbsp;
+          <ArrowRight16 />
+        </span>
+      </a>
     </ClickableTile>
   );
 };
