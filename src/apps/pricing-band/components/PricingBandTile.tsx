483b3752dfa348cd79d1889d3a10f1da14e04e65
Jonathon Westbrook <jonathon.westbrook1@ibm.com>
2022-09-26 19:08:10 -0400

    pricing-band: adds all formatted messages + snapshot

--- src/apps/pricing-band/components/PricingBandTile.tsx
+++ src/apps/pricing-band/components/PricingBandTile.tsx
@@ -10,36 +10,63 @@ const PricingBandTile: React.FC = () => {
   return (
     <ClickableTile
       href="https://www.carbondesignsystem.com/"
-      aria-label="Talk to us"
+      // aria-label={<FormattedMessage {...messages['ctaCopy']} />}
     >
       <div className="expressive-heading-04">
         <FormattedMessage id="c1qDwZ" defaultMessage="FlashSystem 5015" />
       </div>
-      <div className="price expressive-heading-03">Starting at 10, 000</div>
+      <div className="price expressive-heading-03">
+        <span>
+          <FormattedMessage id="Flh+ac" defaultMessage="Starting at " />
+        </span>
+        <span>
+          <FormattedMessage id="HRKWZy" defaultMessage="10,000" />
+        </span>
+      </div>
       <div className="body body-short-01">
-        The 5015 is designed for budget-conscious applications without
-        compromising service, performance or capabilities.
+        <FormattedMessage
+          id="RIViTh"
+          defaultMessage="The 5015 is designed for budget-conscious applications without
+        compromising service, performance or capabilities."
+        />
       </div>
       <ul className="bullets body-short-01">
-        <li>400k IOPS per system</li>
-        <li>8.2 GB/s per system</li>
-        <li>Maximum 32 GB cache</li>
-        <li>Scale-up to 12 PB</li>
+        <li>
+          <FormattedMessage id="NBAKw8" defaultMessage="400K IOPS per system" />
+        </li>
+        <li>
+          <FormattedMessage id="CuL9D1" defaultMessage="8.2 GB/s per system" />
+        </li>
+        <li>
+          <FormattedMessage id="UGudR+" defaultMessage="Maximum 32 GB cache" />
+        </li>
+        <li>
+          <FormattedMessage id="oVQTJi" defaultMessage="Scale-up to 12 PB" />
+        </li>
       </ul>
       <div className="button-group">
         <button className="bx--tag bx--tag--green">
-          <span className="bx--tag__label">Midmarket</span>
+          <span className="bx--tag__label">
+            <FormattedMessage id="plusbl" defaultMessage="Midmarket" />
+          </span>
         </button>
         <button className="bx--tag bx--tag--teal">
-          <span className="bx--tag__label">SAS</span>
+          <span className="bx--tag__label">
+            <FormattedMessage id="EfpVDE" defaultMessage="SAS" />
+          </span>
         </button>
         <br />
         <button className="bx--tag bx--tag--blue">
-          <span className="bx--tag__label">Virtualization</span>
+          <span className="bx--tag__label">
+            <FormattedMessage id="EJ9D2e" defaultMessage="Virtualization" />
+          </span>
         </button>
       </div>
       <span className="cta-copy">
-        <span className="label">Talk to us</span>&nbsp;&nbsp;
+        <span className="label">
+          <FormattedMessage id="aX9C8R" defaultMessage="Talk to us" />
+        </span>
+        &nbsp;&nbsp;
         <ArrowRight16 />
       </span>
     </ClickableTile>
--- src/apps/pricing-band/locales/messages.ts
+++ src/apps/pricing-band/locales/messages.ts
@@ -2,9 +2,46 @@ import { defineMessages } from 'react-intl';
 
 // This contains all the translatable interface messages.
 const messages = defineMessages({
+  ctaCopy: {
+    id: 'aX9C8R',
+    defaultMessage: 'Talk to us',
+  },
   productName: {
     id: 'c1qDwZ',
     defaultMessage: 'FlashSystem 5015',
+  },
+  productPriceLeadingCopy: {
+    id: 'Flh+ac',
+    defaultMessage: 'Starting at ',
+  },
+  productPrice: {
+    id: 'HRKWZy',
+    defaultMessage: '10,000',
+  },
+  productDesc: {
+    id: 'TJMRbW',
+    defaultMessage:
+      'The 5015 is designed for budget-conscious applications without compromising service, performance or capabilities.',
+  },
+  productDescBulletA: {
+    id: 'TJMRbW',
+    defaultMessage:
+      'The 5015 is designed for budget-conscious applications without compromising service, performance or capabilities.',
+  },
+  productDescBulletB: {
+    id: 'TJMRbW',
+    defaultMessage:
+      'The 5015 is designed for budget-conscious applications without compromising service, performance or capabilities.',
+  },
+  productDescBulletC: {
+    id: 'TJMRbW',
+    defaultMessage:
+      'The 5015 is designed for budget-conscious applications without compromising service, performance or capabilities.',
+  },
+  productDescBulletD: {
+    id: 'TJMRbW',
+    defaultMessage:
+      'The 5015 is designed for budget-conscious applications without compromising service, performance or capabilities.',
   },
 });
 
